8e86ace264dabcf70f2fc9c4595f81af89187346
Taras-F <karabasenko@gmail.com>
2020-09-20 17:12:41 +0300

    add Link on Logo

--- src/components/Header/HeaderMiddle/Logo/Logo.jsx
+++ src/components/Header/HeaderMiddle/Logo/Logo.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import style from './Logo.module.css';
 import logoImg from './logo.gif';
+import { Link } from "react-router-dom";
+
 const Logo = ()=>{
     return(
         <div  className = {style.logo}>
-            <a href = "#" alt = "logo"><img src = {logoImg} alt = "logo"/></a>
+            <Link to="/" >
+              <a href = "#" alt = "logo"><img src = {logoImg} alt = "logo"/></a>
+            </Link>
             <span>Мувінг ТУР</span>
         </div>
     )
