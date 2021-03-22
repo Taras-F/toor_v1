017c136e6a0e159579b4411f2e34632f2341fb81
Taras-F <karabasenko@gmail.com>
2021-03-22 22:34:19 +0200

    deleted links

--- src/components/Header/HeaderMiddle/Menu/Prices.jsx
+++ src/components/Header/HeaderMiddle/Menu/Prices.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../Menu/styleForPrices.css';
-import { Link } from "react-router-dom";
 import { useHistory } from 'react-router-dom';
 import ScrollIntoView from 'react-scroll-into-view';
 
